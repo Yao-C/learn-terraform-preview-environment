c3caa86f6d446045d66e7df5d92f2f056b22ac95
Yao Cheng <chengyao19920131@163.com>
2023-03-01 22:10:08 +0100

    Update Vercel project name and TFC organization name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "ychytta"
     workspaces {
       tags = ["hashicupsBackend"]
     }
